d4ead07a4824560bb38f79b2144452b58a92d11d
Abue Ammar <abue.ammar@technonext.com>
2024-03-27 13:00:51 +0600

    ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import SiteHeader from "@/components/site-header";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        <div className="relative flex min-h-screen flex-col bg-background">
+          <SiteHeader />
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ config/data.ts
@@ -0,0 +1,98 @@
+import { MainNavItem, SidebarNavItem } from "@/types/nav";
+
+interface DocsConfig {
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
+}
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs",
+    },
+    {
+      title: "Components",
+      href: "/docs/components/accordion",
+    },
+    {
+      title: "Themes",
+      href: "/themes",
+    },
+    {
+      title: "Examples",
+      href: "/examples",
+    },
+    {
+      title: "Blocks",
+      href: "/blocks",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs",
+          items: [],
+        },
+        {
+          title: "Installation",
+          href: "/docs/installation",
+          items: [],
+        },
+        {
+          title: "components.json",
+          href: "/docs/components-json",
+          items: [],
+        },
+        {
+          title: "Theming",
+          href: "/docs/theming",
+          items: [],
+        },
+        {
+          title: "Dark mode",
+          href: "/docs/dark-mode",
+          items: [],
+        },
+        {
+          title: "CLI",
+          href: "/docs/cli",
+          items: [],
+        },
+        {
+          title: "Typography",
+          href: "/docs/components/typography",
+          items: [],
+        },
+        {
+          title: "Figma",
+          href: "/docs/figma",
+          items: [],
+        },
+        {
+          title: "Changelog",
+          href: "/docs/changelog",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Components",
+      items: [
+        {
+          title: "Accordion",
+          href: "/docs/components/accordion",
+          items: [],
+        },
+        {
+          title: "Alert",
+          href: "/docs/components/alert",
+          items: [],
+        },
+      ],
+    },
+  ],
+};
